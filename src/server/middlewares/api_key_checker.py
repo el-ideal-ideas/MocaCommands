d7202ad9b014a5538dc3549088b1b2791977b1fd
el-ideal-ideas <el.idealideas@gmail.com>
2020-12-18 16:32:33 +0900

    Update

--- src/server/middlewares/api_key_checker.py
+++ src/server/middlewares/api_key_checker.py
@@ -4,6 +4,7 @@ from sanic.request import Request
 from sanic.exceptions import Forbidden, abort
 from limits import parse_many
 from asyncio import sleep
+from sanic.response import text
 from ... import moca_modules as mzk
 
 # -------------------------------------------------------------------------- Imports --
@@ -13,9 +14,12 @@ from ... import moca_modules as mzk
 
 async def api_key_checker(request: Request):
     """A api-key filter."""
+    if request.method.upper() == 'OPTIONS':
+        return text('success.')
     if not request.raw_url.startswith(b'/static') and \
             not request.raw_url.startswith(b'/web') and \
             not request.raw_url.startswith(b'/status') and \
+            not request.raw_url.startswith(b'/moca-commands/static') and \
             request.method.upper() != 'OPTIONS':
         received_key = mzk.get_args(request, ('api_key', str, None, {'max_length': 1024}))[0]
         ip = mzk.get_remote_address(request)
--- src/server/middlewares/force_headers.py
+++ src/server/middlewares/force_headers.py
@@ -2,6 +2,7 @@
 
 from sanic.request import Request
 from sanic.exceptions import Forbidden
+from sanic.response import text
 
 # -------------------------------------------------------------------------- Imports --
 
@@ -10,9 +11,12 @@ from sanic.exceptions import Forbidden
 
 async def force_headers(request: Request):
     """If the request is not contained all headers in `force_headers`, block it."""
+    if request.method.upper() == 'OPTIONS':
+        return text('success.')
     if not request.raw_url.startswith(b'/static') and \
             not request.raw_url.startswith(b'/web') and \
             not request.raw_url.startswith(b'/status') and \
+            not request.raw_url.startswith(b'/moca-commands/static') and \
             request.method.upper() != 'OPTIONS':
         headers = request.app.system_config.get_config('force_headers', dict, {})
         if len(headers) != 0:
